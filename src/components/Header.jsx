402a2fe9c96cd632e0547db0d9ce688748297699
BYCODDE <anaraliev217@gmail.com>
2024-09-09 22:31:18 +0400

    style:header style done!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
 import { UserButton } from "@clerk/clerk-react";
 
-
 function Header() {
   return (
-    <div className="flex justify-between items-center  ">
+    <div className="py-[8px] px-[16px]  border-b border-[#C7CAD0]  flex justify-between items-center  ">
       <img src="./Burger.svg" alt="Burger" className="cursor-pointer" />
-      <UserButton />
+      <UserButton
+        appearance={{
+          elements: {
+            userButtonAvatarBox: {
+              width: "40px",
+              height: "40px",
+            },
+          },
+        }}
+      />
       <div className="flex flx-col justify-center items-start gap-[12px] ">
-        <img src="./Search.svg" alt="Search" className="cursor-pointer" />
-        <div className="w-[1px] h-[16px] bg-[#82868F]"></div>
-        <img src="./Profile.svg" alt="Profile" className="cursor-pointer" />
+        <img
+          src="./Search.svg"
+          alt="Search"
+          className="cursor-pointer w-[20px] h-[20px]"
+        />
+        <div className="w-[1px] h-[20px] bg-[#82868F]"></div>
+        <img
+          src="./Profile.svg"
+          alt="Profile"
+          className="cursor-pointer w-[20px] h-[20px]"
+        />
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import Header from "../components/Header"
 
 function Home() {
   return (
-    <div className="p-[16px]">
+    <div className="p-[16px] bg-[#F6F6F7] w-screen min-h-screen">
       <Header />
     </div>
-  )
+  );
 }
 
 export default Home
